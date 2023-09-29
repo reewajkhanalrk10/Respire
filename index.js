6ad3cd3c1b248936b761195a18a0099d608d8027
abiral-adhikari <109904208+abiral-adhikari@users.noreply.github.com>
2023-09-29 20:00:20 +0545

    changed the location of the setup environment

--- index.js
+++ index.js
@@ -5,18 +5,7 @@ require('dotenv').config()
 
 const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
 
-const environmentSetup=async()=>{
 
-    //Grab your Hedera testnet account ID and private key from your .env file
-    const myAccountId = process.env.MY_ACCOUNT_ID;
-    const myPrivateKey = process.env.MY_PRIVATE_KEY;
-
-    // If we weren't able to grab it, we should throw a new error
-    if (!myAccountId || !myPrivateKey) {
-        throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present");
-    }
-}
-environmentSetup();
 const routes=require('./routes/routes.js')
 app.use('/api',routes)
 app.get('/',(req,res)=>{console.log("Hello World");res.status(200).json({"message":"Hello World"})})
